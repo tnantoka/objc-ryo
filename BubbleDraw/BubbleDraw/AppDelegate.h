9510ce2174764fd87e2033398c1717156767859a
Ryo Miyake <myo.ryo@gmail.com>
2012-08-09 14:43:23 +0900

    BubbleDraw

---
+++ BubbleDraw/BubbleDraw/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BubbleDraw
+//
+//  Created by 三宅 亮 on 12/08/04.
+//  Copyright (c) 2012年 三宅 亮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BubbleDraw/BubbleDraw/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BubbleDraw
+//
+//  Created by 三宅 亮 on 12/08/04.
+//  Copyright (c) 2012年 三宅 亮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
